2119c04ebe8a612965910cce9ad60ada493e3845
Won Tae Lee <maxandjava@gmail.com>
2022-05-31 12:59:56 +0900

    generated 2 rolls in the simulation mesh

--- run.sh
+++ run.sh
@@ -1,11 +1,17 @@
 
-
+foamCleanTutorials
 blockMesh | tee log.blockMesh
-setFields | tee log.setFields
-# interFoam | tee log.solver
+surfaceFeatures | tee log.surfaceFeatures
+snappyHexMesh | tee log.snappyHexMesh
 
-decomposePar
-mpirun -np 8 renumberMesh -overwrite -parallel 
-mpirun -np 8 interFoam -parallel | tee log.solver
-reconstructPar
+
+#setFields | tee log.setFields
+#interFoam | tee log.solver
+
+#decomposePar
+#mpirun -np 8 renumberMesh -overwrite -parallel 
+#mpirun -np 8 interFoam -parallel | tee log.solver
+#reconstructPar
+
+paraFoam
 
