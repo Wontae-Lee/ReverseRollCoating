293629eff2cb00fd3c6d7e80364578bceade895d
Won Tae Lee <maxandjava@gmail.com>
2022-06-05 18:19:12 +0900

    add rotatiting right roll

--- run.sh
+++ run.sh
@@ -1,17 +1,24 @@
-
 foamCleanTutorials
-blockMesh | tee log.blockMesh
-surfaceFeatures | tee log.surfaceFeatures
-snappyHexMesh | tee log.snappyHexMesh
+rm -rf 0 > /dev/null 2>&1
+#cp -r 0_org 0 > /dev/null 2>&1
 
 
-#setFields | tee log.setFields
-#interFoam | tee log.solver
 
-#decomposePar
-#mpirun -np 8 renumberMesh -overwrite -parallel 
-#mpirun -np 8 interFoam -parallel | tee log.solver
-#reconstructPar
+surfaceFeatures
+blockMesh
+decomposePar
+mpirun -np 8 snappyHexMesh -parallel -overwrite | tee log.shm
+#reconstructParMesh -mergeTol 1e-06 -constant
+reconstructParMesh -constant
+
+# #If you dont split the mesh and create the AMI patches it will diverge
+# createBaffles -overwrite	| tee log.createBaffles
+# splitBaffles -overwrite		| tee log.splitBaffles
+# createPatch -overwrite
+# splitMeshRegions -detectOnly	| tee log.splitMeshRegions	
+
+
+#transformPoints 'scale = (0.01 0.01 0.01)'
+# checkMesh | tee log.checkmesh
 
 paraFoam
-
