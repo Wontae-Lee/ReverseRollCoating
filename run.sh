5749d6b119befec7cffc36c01bdec71a0676167f
Won Tae Lee <maxandjava@gmail.com>
2022-06-14 19:45:36 +0900

    bugfix

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 
-
 decomposePar -force
-mpirun -np 8 interFoam > log.interFoam -parallel
+#mpirun -np 8 interFoam > log.interFoam -parallel
+mpirun -np 4 interFoam  > /dev/null -parallel
 reconstructPar
 
 #------------------------------------------------------------------------------
